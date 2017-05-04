e98dae85833ab3adafcc90d3cd4e4bd93e655c56
Overflow012 <cristhianboujon@gmail.com>
2017-05-04 17:39:50 -0300

    logger added to feeds_input

--- feeds_input.py
+++ feeds_input.py
@@ -7,10 +7,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from feeds_model import *
 from db import DBSession
+import logging
+import datetime as dtt
+
+
+# start - logging configuration
+# @TODO: Maybe logging in this way it is an error sinse 
+# https://docs.python.org/3.5/howto/logging-cookbook.html#logging-to-a-single-file-from-multiple-processes
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+log_formatter = logging.Formatter('[%(asctime)s] %(message)s')
+logger_handler = logging.FileHandler("logs/{0}_feeds_input.log".format(dtt.datetime.today().strftime("%Y-%m-%d")))
+logger_handler.setFormatter(log_formatter)
+logger.addHandler(logger_handler)
+# end - logging configuration
 
 def process_feed(_url, download_folder):
 
-    file_name = ""
+    logger = logging.getLogger(__name__)
 
     feed_in = DBSession().query(FeedIn).filter_by(url = _url ).one()
 
@@ -20,47 +34,14 @@ def process_feed(_url, download_folder):
         file_name = download_file(url, download_folder)
         result = preprocess(file_name, feed_in.bulk_insert, ())
         for res in result:
-            print( res)
-            """            db_log(db_connection, 
-                            url = url, 
-                            file = file_name, 
-                            status = res['status'], 
-                            inserted = res['inserted'],
-                            e_msg = res['e_msg'])"""
+            logger.info("{0} {1} {2} {3} {4}".format(url, file_name, res['status'], res['inserted'], res['e_msg']))
+    
     except Exception as e:
-        print(e)
-        """db_log(db_connection, 
-            url = url, 
-            file = file_name, 
-            status = type(e).__name__, 
-            e_msg = str(e))"""
+        file_name = ""
+        logger.info("{0} {1} {2} {3} {4}".format(url, file_name, type(e).__name__, 0, str(e)))
 
 
-def db_log(db_connection, **kwargs):
-    field_list = ("url", "file", "status", "inserted", "e_msg")
-    value_list = []
 
-    sql = """INSERT INTO fp_feeds_in_log (%s, %s, %s, %s, %s) 
-                VALUES (%s, %s, %s, %s, %s)"""
-    
-    for field in field_list:
-        if (field not in kwargs) or (kwargs[field] == "" or kwargs[field] == None):
-            value = "NULL"
-        elif(type(kwargs[field]) == str):
-            value = db_connection.escape(kwargs[field])
-        else:
-            value = kwargs[field]
-
-        value_list.append(value)
-
-    
-    value_list = tuple(value_list)
-    sql = sql % (field_list + value_list)
-
-    with db_connection.cursor() as cursor:
-        cursor.execute(sql)
-
-    db_connection.commit() 
 
 def run(download_folder, urls, num_workers = None):
 
@@ -86,6 +67,7 @@ if __name__ == '__main__':
         ,'http://fotoautos.com/xml/trovit/trovit_co.xml'
         ,'http://fotoautos.com/xml/trovit/trovit_mx.xml'
         ,'http://fotoautos.com/xml/trovit/trovit_pe.xml'
+        ,'http://inmueblesenweb.com/xml-trovit/'
     ]
 
     #urls = ['http://allhouses.com.br/feeds/trovit']
